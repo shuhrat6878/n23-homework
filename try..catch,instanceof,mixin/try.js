04fa1c358eaca107a977f93f4b36014f3652a1fa
shuhrat6878 <abbosovich1999@gmail.com>
2025-06-04 08:19:08 +0500

    new

---
+++ try..catch,instanceof,mixin/try.js
@@ -0,0 +1,73 @@
+// 1- masala
+
+
+// class Student {
+//     constructor(name, age, grade) {
+//         this.name = name;
+//         this.age = age;
+//         this.grade = grade;
+//     }
+
+//     getInfo() {
+//         return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+//     }
+// }
+
+// let talaba = new Student('Shuxrat', 34, 'A');
+
+// console.log(talaba.getInfo());
+
+
+
+// 2- masala
+
+// class Car {
+//     constructor(brand, model, year) {
+//         this.brand = brand;
+//         this.model = model;
+//         this.year = year
+//     }
+//     getCarAge(year) {
+//         return 2025 - this.year;
+//     }
+//     isOlderCar(year) {
+//         let res = 2025 - this.year;
+//         if (res > 10) return "eski mashina";
+//         if(res<10)return "yangi mashina"
+        
+//     }
+// }
+
+// let mashina = new Car("chevralet","taxo",2020);
+
+// mashina.isOlderCar()
+// console.log(mashina.getCarAge());
+// console.log(mashina.isOlderCar());
+
+
+// 3- masala
+
+
+// 4-masala
+
+// class Animal {
+//     constructor(name, sound) {
+//         this.name = name;
+//         this.sound = sound;
+//     }
+
+//     makeSound() {
+//         console.log(this.sound);
+//     }
+// }
+// class Dog extends Animal { }
+// let dog1 = new Dog("bobik", "wof");
+// dog1.makeSound(); 
+// console.log(dog1); 
+
+
+// class Cat extends Animal { }
+// let cat1 = new Cat("jery", "meow");
+// cat1.makeSound(); 
+// console.log(cat1); 
+

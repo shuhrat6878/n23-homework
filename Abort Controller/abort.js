3883d612f4e60142208aa4ff3312d8dd7547afdc
shuhrat6878 <abbosovich1999@gmail.com>
2025-06-14 01:10:26 +0500

    new

--- Abort Controller/abort.js
+++ Abort Controller/abort.js
@@ -1,58 +1,165 @@
+const author_URL = "https://6849a33445f4c0f5ee724f2c.mockapi.io/author";
+const book_URL = "https://6849a33445f4c0f5ee724f2c.mockapi.io/book";
 
-const Url = 'https://jsonplaceholder.typicode.com/users'
+class Book {
+  
+  getBooks = async () => {
+    const res = await fetch(book_URL);
+    const data = await res.json();
+    return data;
+  };
 
-async function getUsers() {
+  
+  addBook = async (book) => {
+    await fetch(book_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(book),
+    });
+
+    console.log(await this.getBooks());
+  };
+
+ 
+  update = async (id, book) => {
     try {
-        const res = await fetch(Url)
-        const data = await res.json();
-        return data;
+      const res = await fetch(`${book_URL}/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(book),
+      });
+      const data = await res.json();
+      console.log(data);
     } catch (error) {
-        console.log('eror users',error)
+      console.log(error);
     }
-    
-}
-// getUsers().then(console.log)
+  };
 
-
-async function getIdUsers(id) {
+  
+  remove = async (id) => {
     try {
-        const res = await fetch(`${Url}/${id}`)
-        const data = await res.json();
-        return data;
+      await fetch(`${book_URL}/${id}`, {
+        method: "DELETE",
+      });
+      console.log("Deleted successfully");
     } catch (error) {
-        console.log('ereor id ',error);
-        
+      console.log(error);
     }
-    
+  };
 }
+
+class Author {
+  constructor(name, password) {
+    this.name = name;
+    this.password = password;
+  }
+
+  
+  createUser = async () => {
+    try {
+      const res = await fetch(author_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: this.name,
+          password: this.password,
+          books: [], 
+        }),
+      });
+
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  async getBooks(authorId) {
+    const res = await fetch(`${author_URL}/${authorId}`);
+    const data = await res.json();
+    return data.books || [];
+  }
+
+  
+  async addBook(bookId, authorId) {
+    const books = await book.getBooks();
+    const bookItem = books.find((b) => b.id == bookId);
+
+    if (!bookItem) {
+      console.log("Kitob topilmadi!");
+      return;
+    }
+
+    const authorBooks = await this.getBooks(authorId);
+
+    await fetch(`${author_URL}/${authorId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ books: [...authorBooks, bookId] }),
+    });
+
+    console.log(`kitobni id-si ${bookId} muallif ni id-siga ${authorId} ga qoshildi`);
+  }
+}
+
+const book = new Book();
+
+// book.addBook({ title: "Otkan kunlar", description: "Abdulla Qodiriy" });
+// book.addBook({ title: "Yulduzli tunlar", description: "Pirimqul Qodirov" });
+// book.addBook({ title: "Sariq devni minib", description: "Xudoyberdi To‘xtaboyev" });
+
+const ali = new Author("Ali", "454545");
+// ali.createUser();
+
+
+ali.addBook(1, 1).then(() => console.log("tayyorlandi"));
+
+
+
+
+
+
+
+
+
+
+
+// const Url = 'https://jsonplaceholder.typicode.com/users'
+
+// async function getUsers() {
+//     try {
+//         const res = await fetch(Url)
+//         const data = await res.json();
+//         return data;
+//     } catch (error) {
+//         console.log('eror users',error)
+//     }
+
+// }
+// // getUsers().then(console.log)
+
+// async function getIdUsers(id) {
+//     try {
+//         const res = await fetch(`${Url}/${id}`)
+//         const data = await res.json();
+//         return data;
+//     } catch (error) {
+//         console.log('ereor id ',error);
+
+//     }
+
+// }
 // getIdUsers(4).then(console.log)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // const controller = new AbortController();
-
 
 // async function getData() {
 //     try {
@@ -63,7 +170,7 @@ async function getIdUsers(id) {
 //     } catch (error) {
 //         console.log('hatolik wuyerda cqadi--=---',error)
 //     }
-    
+
 // }
 // getData();
 
---
+++ masalalar/masala.js
@@ -0,0 +1,30 @@
+// let arr = [1,2,3];
+//  let  natija  = []
+
+// for (let i = 0; i < arr.length; i++){
+//     console.log("a---",i)
+//     for (let j = 1; j <= arr.length; j++){
+//         console.log("b-",j);
+        
+//     }
+// }
+
+//222
+
+// let son = [0, 1, 0, 3, 12];
+// let boxx =0
+
+// for (let i = 0; i < son.length; i++){
+//     if (son[i] !== 0) {
+//         son[boxx] = son[i];
+//         boxx++;
+//     }
+
+// }
+
+// while (boxx < son.length) {
+//     son[boxx] = 0
+//     boxx++;
+
+// }
+// console.log(son);
